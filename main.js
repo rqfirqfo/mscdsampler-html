5600ce06ed87bb7ae65ec94b0bf4ae03814da428
rqfirqfo <doanything414@gmail.com>
2025-06-20 19:18:17 +0700

    okay sure

--- main.js
+++ main.js
@@ -4,9 +4,14 @@ $(document).ready(function(){
     var container = $('.container');
     var categories = $('.category');
 
-    startVideo.play();
+    $('#playbt').click(function(){
+        startVideo.play();
+        $('#playbt').hide();
+        $('#start-video').css('display', 'block');
+        $('.disclaimer-text').css('display', 'block');
+    });
 
-    // remove this if finished
+    // to skip the video intros, will be removed later after i finish
     $('.disclaimer-text').click(function(){
         stopStartup();
     });
@@ -15,7 +20,7 @@ $(document).ready(function(){
         startVideo.src = "video/intro.mp4";
         startVideo.load();
         startVideo.play();
-        $('.disclaimer-text').css('display', 'none');
+        $('.disclaimer-text').hide();
         $('.skip-text').css('display', 'block');
 
         $('.skip-text').click(function(){
@@ -29,61 +34,100 @@ $(document).ready(function(){
         
     function stopStartup() {
         startVideo.pause();
-        backgroundAudio.play();
         startVideo.src = "";
-        $('.startup').css('display', 'none');
+        backgroundAudio.play();
+        $('.startup').hide();
         container.fadeIn(300);
         setTimeout(function() {
-            $('.wdywtgt').fadeOut(200);
+            $('.wdywtgt').fadeOut(500);
         }, 8000);
     }
-            backgroundAudio.onended = function(e) {
-                mainmenu();
-            }
+
+    function mainBGM() {
+        backgroundAudio.pause();
+        backgroundAudio.src = 'sounds/MAIN.WAV'
+        backgroundAudio.load();
+        backgroundAudio.play();
+    };
+    
+    backgroundAudio.onended = function(e) {
+        mainmenu();
+    }
 
             function mainmenu() {
                 backgroundAudio.pause();
-                backgroundAudio.src = "sounds/MAIN.WAV";
                 $('.homepage').css('display', 'block');
                 $('.category').each(function(index) {
                     setTimeout(function() {
                         $(this).css('display', 'block');
                         if (index === categories.length - 1) {
-                            backgroundAudio.load();
-                            backgroundAudio.play();
+                                mainBGM();
                             $('.bottom-nav').css('display', 'flex');
-
                         }
                     }.bind(this), 200 * index);
                 });
             }
+
+            $('.accessory-button').hover(function () {
+                setTimeout(function() {
+                    backgroundAudio.pause();
+                    backgroundAudio.src = 'sounds/NAR17.WAV'
+                    backgroundAudio.play();
+                }, 1000);
+                }, function () {
+                    setTimeout(function() {
+                        mainBGM();
+                    }, 2000);   
+                }
+            );
+
+            $('.exit-button').hover(function () {
+                setTimeout(function() {
+                    backgroundAudio.pause();
+                    backgroundAudio.src = 'sounds/NAR19.WAV'
+                    backgroundAudio.play();
+                }, 1000);
+                    
+                }, function () {
+                    setTimeout(function() {
+                        mainBGM();
+                    }, 2000);   
+                }
+            );
+
+            $('.intro-button').hover(function () {
+                setTimeout(function() {
+                    backgroundAudio.pause();
+                    backgroundAudio.src = 'sounds/NAR18.WAV'
+                    backgroundAudio.play();
+                }, 1000);
+                    
+                }, function () {
+                }
+            );
+
+            $('.intro-button').click(function(){
+                backgroundAudio.pause();
+                backgroundAudio.src = "";
+                backgroundAudio.load();
+                container.hide();
+                setTimeout(function(){
+                    $('.startup').show();
+                    $('#start-video').show();
+                    startVideo.src = "video/mslogo.mp4";
+                    startVideo.play();
+                    $('.disclaimer-text').css('display', 'block');
+                }, 1000);
+            });
     
     $('.main-button').click(function(){
         backgroundAudio.pause();
-        backgroundAudio.src = "sounds/MAIN.WAV"
         $('.category-page').fadeOut(200, function(){
             $('.homepage').fadeIn(200);
-            backgroundAudio.load();
-            backgroundAudio.play();
+                mainBGM();
         });
         backgroundAudio.onended = function(e) {
-            backgroundAudio.src = "sounds/MAIN.WAV"
-            backgroundAudio.play();
-        }    
-    });
-
-    $('[class="category kids"]').click(function(){
-        backgroundAudio.pause();
-        backgroundAudio.src = "sounds/NAR20.WAV"
-        $('.homepage').fadeOut(100, function(){
-            $('.category-page').fadeIn(200);
-            backgroundAudio.load();
-            backgroundAudio.play();
-        });
-        $('.category-page').css('background-image', 'url(images/KIDS.BMP)')
-        backgroundAudio.onended = function(e) {
-            backgroundAudio.src = "sounds/KIDWORLD.WAV"
-            backgroundAudio.play();
+            mainBGM();
         }    
     });
 
